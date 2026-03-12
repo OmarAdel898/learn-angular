b2d4897eb052caa1d01ba527b86ce010e6c73b36
OmarAdel898 <omar.97453@cs.mti.edu.eg>
2026-03-12 13:49:12 +0200

    day 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', loadComponent: () => import('../components/home/home').then(m => m.Home)},
+    {path: 'products', loadComponent: () => import('../components/products/products').then(m => m.Products)},
+    {path: 'contact', loadComponent: () => import('../components/contact/contact').then(m => m.Contact)},
+    {path: '**', loadComponent: () => import('../components/notfound/notfound').then(m => m.Notfound)},
+];
--- src/components/products/products.ts
+++ src/components/products/products.ts
@@ -7,36 +7,42 @@ import { Component } from '@angular/core';
   styleUrl: './products.css',
 })
 export class Products {
+  // products = [
+  //   {
+  //     name: 'Wireless Headphones',
+  //     price: 79.99,
+  //     image: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=300&h=300&fit=crop',
+  //   },
+  //   {
+  //     name: 'Running Sneakers',
+  //     price: 119.99,
+  //     image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=300&h=300&fit=crop',
+  //   },
+  //   {
+  //     name: 'Smart Watch',
+  //     price: 199.99,
+  //     image: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=300&h=300&fit=crop',
+  //   },
+  //   {
+  //     name: 'Leather Backpack',
+  //     price: 89.99,
+  //     image: 'https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=300&h=300&fit=crop',
+  //   },
+  //   {
+  //     name: 'Sunglasses',
+  //     price: 49.99,
+  //     image: 'https://images.unsplash.com/photo-1572635196237-14b3f281503f?w=300&h=300&fit=crop',
+  //   },
+  //   {
+  //     name: 'Mechanical Keyboard',
+  //     price: 149.99,
+  //     image: 'https://images.unsplash.com/photo-1587829741301-dc798b83add3?w=300&h=300&fit=crop',
+  //   },
+  // ];
+
   products = [
-    {
-      name: 'Wireless Headphones',
-      price: 79.99,
-      image: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=300&h=300&fit=crop',
-    },
-    {
-      name: 'Running Sneakers',
-      price: 119.99,
-      image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=300&h=300&fit=crop',
-    },
-    {
-      name: 'Smart Watch',
-      price: 199.99,
-      image: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=300&h=300&fit=crop',
-    },
-    {
-      name: 'Leather Backpack',
-      price: 89.99,
-      image: 'https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=300&h=300&fit=crop',
-    },
-    {
-      name: 'Sunglasses',
-      price: 49.99,
-      image: 'https://images.unsplash.com/photo-1572635196237-14b3f281503f?w=300&h=300&fit=crop',
-    },
-    {
-      name: 'Mechanical Keyboard',
-      price: 149.99,
-      image: 'https://images.unsplash.com/photo-1587829741301-dc798b83add3?w=300&h=300&fit=crop',
-    },
+    { id: 1, name: 'Smart Watch', price: 120, image: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=300&h=300&fit=crop', inStock: true },
+    { id: 2, name: 'Classic Watch', price: 90, image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=300&h=300&fit=crop', inStock: false },
+    { id: 3, name: 'Sport Watch', price: 150, image: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=300&h=300&fit=crop', inStock: true }
   ];
 }
